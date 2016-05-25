707ebedb4319e7a948ecf115d47b7dc4e823c843
cooperoelrichs <c.oelrichs@gmail.com>
2016-05-25 11:16:47 +1000

    1. Turn plotting prototype code into a usable class; 2. add financial calculations; and 3. add printable summary.

---
+++ results_plotter.py
@@ -0,0 +1,116 @@
+import numpy as np
+
+# %matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+from matplotlib.collections import LineCollection
+
+
+matplotlib.rcParams['figure.figsize'] = (10.0, 8.0)
+# plt.style.use('ggplot')
+
+
+def to_rgba(rgb):
+    return tuple([x / 255 for x in rgb])
+
+
+grey_colour = to_rgba((85, 85, 85))
+
+plot_formats = {
+    'AR': {
+        'colour': to_rgba((226, 74, 51)),  # E24A33
+        'label': 'Cumulative Amount Repayed'
+    },
+
+    'AO': {
+        'colour': to_rgba((166, 6, 40)),  # A60628
+        'label': 'Amount Owing on Loan'
+    },
+
+    'CR': {
+        'colour': to_rgba((52, 138, 189)),  # 348ABD
+        'label': 'Cumulative Revenue'
+    },
+
+    'CP': {
+        'colour': to_rgba((70, 120, 33)),  # 467821
+        'label': 'Cumulative Profit'
+    }
+}
+
+
+class ResultsPlotter(object):
+    def __init__(self,
+                 amount_repayed, amount_owing,
+                 cumulative_revenue, cumulative_profit,
+                 labels):
+        self.amount_repayed = amount_repayed
+        self.amount_owing = amount_owing
+        self.cumulative_revenue = cumulative_revenue
+        self.cumulative_profit = cumulative_profit
+        self.formats = plot_formats
+        self.labels = labels
+
+    def plot_and_savefig(self, file_name):
+        self.plot_it()
+        plt.savefig(file_name, bbox_inches='tight')
+
+    def plot_it(self):
+        ax = plt.subplot(1, 1, 1)
+        ax = self.add_series_and_fill(ax, self.amount_repayed, 'AR', -1)
+        ax = self.add_series_and_fill(ax, self.amount_owing, 'AO', 0)
+        ax = self.add_series_and_fill(ax, self.cumulative_revenue, 'CR', -1)
+        ax = self.add_series_and_fill(ax, self.cumulative_profit, 'CP', -1)
+
+        plt.title('Oh Cabins - Comparison of Different Scenarios')
+        plt.xlabel('Months')
+        yl = plt.ylabel('$  ')
+
+        legend = plt.legend(
+            loc="upper left",
+            # frameon=False,
+            bbox_to_anchor=(0, 1)
+        )
+
+        for text in legend.get_texts():
+            plt.setp(text, color=grey_colour)  # fontsize=10
+
+        yl.set_rotation(0)
+        ax.get_yaxis().set_major_formatter(
+            FuncFormatter(lambda x, p: format(x, ',.0f'))
+        )
+        # ax.tick_params(axis='both', which='both', length=0)
+        ax.autoscale_view()
+
+    def add_series_and_fill(self, ax, series, name, max_axis):
+        x = np.array(range(series.shape[1]), dtype='float64')
+
+        collection = self.make_line_collection(
+            x, series, self.formats[name]
+        )
+        ax.add_collection(collection, autolim=True)
+
+        areas = np.trapz(series)
+        y_max = series[np.argmax(areas)]
+        y_min = series[np.argmin(areas)]
+        ax.fill_between(
+            x, y_min, y_max,
+            facecolor=self.formats[name]['colour'], alpha=0.2
+        )
+        return ax
+
+    def make_line_collection(self, x, series, format_):
+        # series [series, y]
+        # Lines [series, x, y]
+        lines = np.zeros((series.shape[0], series.shape[1], 2))
+        lines[:, :, 0] = x
+        lines[:, :, 1] = series
+
+        lc = LineCollection(
+            lines,
+            colors=format_['colour'],
+            label=format_['label'],
+            alpha=0.5
+        )
+        return lc
--- scenario_testing.py
+++ scenario_testing.py
@@ -1,6 +1,8 @@
 import itertools
 import numpy as np
 
+from .results_plotter import ResultsPlotter
+
 
 class Scenario(object):
     def __init__(self,
@@ -15,19 +17,50 @@ class Scenario(object):
         r = self.montly_interest()
         p = self.project_cost
         n = self.number_of_periods()
+
         c = r * p / (1 - (1 + r) ** -n)
         return c
 
     def calculate_amount_repayed_by_month(self):
-        X = np.empty((self.number_of_periods() + 1))
-        X[:] = self.calculate_repayment_amount()
+        # X = np.ones(self.number_of_periods() + 1)
+        X = self.cumulative_periods_vector()
+        X *= self.calculate_repayment_amount()
         return X
+
+    def calculate_amount_owing_by_month(self):
+        r = self.montly_interest()
+        P = np.zeros(self.number_of_periods() + 1, dtype='float64')
+        P[:] = self.project_cost
+        N = self.cumulative_periods_vector()
+        c = self.calculate_repayment_amount()
+
+        P_f = P * ((1 + r) ** N) - c * (((1 + r) ** N - 1) / r)
+        return P_f
+
+    def calculate_cumulative_revenue_by_month(self):
+        X = self.cumulative_periods_vector()
+        X *= self.montly_revenue()
+        return X
+
+    def cumulative_periods_vector(self):
+        return np.array(range(self.number_of_periods() + 1), dtype='float64')
 
     def montly_interest(self):
         return self.annual_interest_rate / 12
 
+    def montly_revenue(self):
+        return self.annual_revenue / 12
+
     def number_of_periods(self):
         return self.loan_term_in_years * 12
+
+    def label(self):
+        return {
+            'project_cost': self.project_cost,
+            'loan_term_in_years': self.loan_term_in_years,
+            'annual_interest_rate': self.annual_interest_rate,
+            'annual_revenue': self.annual_revenue
+        }
 
 
 class ScenarioCollection(object):
@@ -60,26 +93,33 @@ class ScenarioTester(object):
         self.scenarios = scenarios
 
     def test(self):
-        repayment_amount = self.calculate_repayment_amount()
-        amount_repayed_by_month = self.calculate_amount_repayed_by_month()
-        amount_owing_by_month = self.calculate_amount_owing_by_month()
+        amount_repayed_by_month = self.calculate_amounts_repayed_by_month()
+        amount_owing_by_month = self.calculate_amounts_owing_by_month()
+        cumulative_revenue = self.calculate_cumulative_revenues_by_month()
+        cumulative_profit = cumulative_revenue - amount_repayed_by_month
 
         results = ScenarioTestResults(
-            repayment_amount, amount_repayed_by_month,
-            amount_owing_by_month,
-            cumulative_revenue, cumulative_profit
+            amount_repayed_by_month, amount_owing_by_month,
+            cumulative_revenue, cumulative_profit,
+            self.labels()
         )
         return results
 
-    def calculate_repayment_amount(self):
+    def calculate_repayment_amounts(self):
         fns = [s.calculate_repayment_amount for s in self.scenarios]
-        repayment_amount = self.calculate_from_function(fns)
-        return repayment_amount
+        return self.calculate_from_function(fns)
 
-    def calculate_amount_repayed_by_month(self):
+    def calculate_amounts_repayed_by_month(self):
         fns = [s.calculate_amount_repayed_by_month for s in self.scenarios]
-        amount_repayed = self.calculate_from_function(fns)
-        return amount_repayed
+        return self.calculate_from_function(fns)
+
+    def calculate_amounts_owing_by_month(self):
+        fns = [s.calculate_amount_owing_by_month for s in self.scenarios]
+        return self.calculate_from_function(fns)
+
+    def calculate_cumulative_revenues_by_month(self):
+        fns = [s.calculate_cumulative_revenue_by_month for s in self.scenarios]
+        return self.calculate_from_function(fns)
 
     def calculate_from_function(self, fns):
         X = np.array([
@@ -87,19 +127,46 @@ class ScenarioTester(object):
         ])
         return X
 
+    def labels(self):
+        return [s.label for s in self.scenarios]
+
 
 class ScenarioTestResults(object):
     def __init__(self,
-                 amount_payed, amount_owing,
-                 cumulative_revenue, cumulative_profit):
-        self.amount_payed = amount_payed
+                 amount_repayed, amount_owing,
+                 cumulative_revenue, cumulative_profit,
+                 labels):
+        self.amount_repayed = amount_repayed
         self.amount_owing = amount_owing
         self.cumulative_revenue = cumulative_revenue
         self.cumulative_profit = cumulative_profit
+
+        self.plotter = ResultsPlotter(
+            self.amount_repayed, self.amount_owing,
+            self.cumulative_revenue, self.cumulative_profit,
+            labels
+        )
         pass
 
     def summarise(self):
-        pass
+        print(self.summary_str(
+            'Total cost', self.amount_owing[:, 0]))
+        print(self.summary_str(
+            'Monthly repayment', self.amount_repayed[:, 1]))
+        print(self.summary_str(
+            'Monthly revenue', self.cumulative_revenue[:, 1]))
+        print(self.summary_str(
+            'Monthly profit', self.cumulative_profit[:, 1]))
+        print(self.summary_str(
+            'Total profit', self.cumulative_profit[:, -1]))
 
     def plot(self):
-        pass
+        self.plotter.plot_and_savefig('results.png')
+
+    def summary_str(self, name, X):
+        v_min = np.min(X)
+        v_max = np.max(X)
+        return self.min_max_str(name, v_min, v_max)
+
+    def min_max_str(self, name, v_min, v_max):
+        return '{0}: ${1:,.0f} - ${2:,.0f}'.format(name, v_min, v_max)
