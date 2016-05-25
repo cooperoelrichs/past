7af41d9298b638444f84bce4c8354c4472f277d5
cooperoelrichs <c.oelrichs@gmail.com>
2016-05-25 11:18:51 +1000

    Add support for a lead time, an x-axis in years, and variable loan terms.

--- results_plotter.py
+++ results_plotter.py
@@ -58,13 +58,15 @@ class ResultsPlotter(object):
 
     def plot_it(self):
         ax = plt.subplot(1, 1, 1)
+
+        ax = self.add_zero_line(ax, self.amount_repayed)
         ax = self.add_series_and_fill(ax, self.amount_repayed, 'AR', -1)
         ax = self.add_series_and_fill(ax, self.amount_owing, 'AO', 0)
         ax = self.add_series_and_fill(ax, self.cumulative_revenue, 'CR', -1)
         ax = self.add_series_and_fill(ax, self.cumulative_profit, 'CP', -1)
 
         plt.title('Oh Cabins - Comparison of Different Scenarios')
-        plt.xlabel('Months')
+        plt.xlabel('Years')
         yl = plt.ylabel('$  ')
 
         legend = plt.legend(
@@ -84,19 +86,17 @@ class ResultsPlotter(object):
         ax.autoscale_view()
 
     def add_series_and_fill(self, ax, series, name, max_axis):
-        x = np.array(range(series.shape[1]), dtype='float64')
-
+        x = self.make_x(series)
         collection = self.make_line_collection(
             x, series, self.formats[name]
         )
         ax.add_collection(collection, autolim=True)
 
-        areas = np.trapz(series)
-        y_max = series[np.argmax(areas)]
-        y_min = series[np.argmin(areas)]
+        y_max = np.max(series, axis=0)
+        y_min = np.min(series, axis=0)
         ax.fill_between(
             x, y_min, y_max,
-            facecolor=self.formats[name]['colour'], alpha=0.2
+            facecolor=self.formats[name]['colour'], alpha=0.4
         )
         return ax
 
@@ -111,6 +111,16 @@ class ResultsPlotter(object):
             lines,
             colors=format_['colour'],
             label=format_['label'],
-            alpha=0.5
+            alpha=1
         )
         return lc
+
+    def add_zero_line(self, ax, series):
+        x = self.make_x(series)
+        ax.plot(x, x, c=grey_colour, linewidth=2, alpha=0.7, zorder=1)
+        return ax
+
+    def make_x(self, series):
+        x = np.array(range(series.shape[1]), dtype='float64')
+        x /= 12
+        return x
--- scenario_testing.py
+++ scenario_testing.py
@@ -6,31 +6,36 @@ from .results_plotter import ResultsPlotter
 
 class Scenario(object):
     def __init__(self,
-                 project_cost, loan_term_in_years,
-                 annual_interest_rate, annual_revenue):
-        self.project_cost = project_cost
-        self.loan_term_in_years = loan_term_in_years
+                 principle, annual_interest_rate,
+                 loan_term_in_years, lead_time_in_years,
+                 revenue_unit, annual_revenue_factor,
+                 max_loan_term):
+        self.principle = principle
         self.annual_interest_rate = annual_interest_rate
-        self.annual_revenue = annual_revenue
+        self.loan_term_in_years = loan_term_in_years
+        self.lead_time_in_years = lead_time_in_years
+        self.revenue_unit = revenue_unit
+        self.annual_revenue = revenue_unit * annual_revenue_factor
+        self.max_loan_term = max_loan_term
 
     def calculate_repayment_amount(self):
         r = self.montly_interest()
-        p = self.project_cost
+        p = self.principle
         n = self.number_of_periods()
 
         c = r * p / (1 - (1 + r) ** -n)
         return c
 
     def calculate_amount_repayed_by_month(self):
-        # X = np.ones(self.number_of_periods() + 1)
         X = self.cumulative_periods_vector()
+        X[self.number_of_periods() + 1:] = np.max(X)
         X *= self.calculate_repayment_amount()
         return X
 
     def calculate_amount_owing_by_month(self):
         r = self.montly_interest()
-        P = np.zeros(self.number_of_periods() + 1, dtype='float64')
-        P[:] = self.project_cost
+        P = np.zeros(self.max_num_periods() + 1, dtype='float64')
+        P[:self.number_of_periods() + 1] = self.principle
         N = self.cumulative_periods_vector()
         c = self.calculate_repayment_amount()
 
@@ -38,12 +43,26 @@ class Scenario(object):
         return P_f
 
     def calculate_cumulative_revenue_by_month(self):
-        X = self.cumulative_periods_vector()
+        X = np.array(
+            range(self.max_num_periods() + 1),
+            dtype='float64'
+        )
+        # X[self.number_of_periods() + 1:] = np.max(X)
+        X -= self.lead_time_in_months()
+        X[X < 0] = 0
         X *= self.montly_revenue()
         return X
 
     def cumulative_periods_vector(self):
-        return np.array(range(self.number_of_periods() + 1), dtype='float64')
+        cumulative_periods = np.zeros(
+            self.max_num_periods() + 1,
+            dtype='float64'
+        )
+        cumulative_periods[:self.number_of_periods() + 1] = np.array(
+            range(self.number_of_periods() + 1),
+            dtype='float64'
+        )
+        return cumulative_periods
 
     def montly_interest(self):
         return self.annual_interest_rate / 12
@@ -54,38 +73,56 @@ class Scenario(object):
     def number_of_periods(self):
         return self.loan_term_in_years * 12
 
+    def lead_time_in_months(self):
+        return self.lead_time_in_years * 12
+
+    def max_num_periods(self):
+        return self.max_loan_term * 12
+
     def label(self):
         return {
-            'project_cost': self.project_cost,
-            'loan_term_in_years': self.loan_term_in_years,
-            'annual_interest_rate': self.annual_interest_rate,
-            'annual_revenue': self.annual_revenue
+            'principle': self.principle,
+            'loan_term': self.loan_term_in_years,
+            'interest_rate': self.annual_interest_rate,
+            'revenue': self.revenue_unit,
+            'aror': self.annual_revenue / self.principle
         }
 
 
 class ScenarioCollection(object):
     def __init__(self,
-                 project_costs, loan_term_in_years,
-                 annual_interest_rates, annual_revenues):
-        self.project_costs = project_costs
-        self.loan_term_in_years = loan_term_in_years
+                 principles, annual_interest_rates, loan_terms_in_years,
+                 lead_times_in_years, revenue_units,
+                 annual_revenue_factor):
+        self.principles = principles
         self.annual_interest_rates = annual_interest_rates
-        self.annual_revenues = annual_revenues
+        self.loan_terms_in_years = loan_terms_in_years
+        self.lead_times_in_years = lead_times_in_years
+        self.revenue_units = revenue_units
+        self.annual_revenue_factor = annual_revenue_factor
+
         self.scenarios = self.combine()
 
     def combine(self):
         scenarios = [
-            Scenario(pc, lt, ai, ar)
-            for pc, lt, ai, ar in itertools.product(
-                self.project_costs,
-                [self.loan_term_in_years],
+            Scenario(
+                p, ir, lt, ld, ru,
+                self.annual_revenue_factor, self.max_load_length()
+            )
+            for p, ir, lt, ld, ru in itertools.product(
+                self.principles,
                 self.annual_interest_rates,
-                self.annual_revenues
+                self.loan_terms_in_years,
+                self.lead_times_in_years,
+                self.revenue_units
             )
         ]
 
         print('Created %i scenarios.' % len(scenarios))
         return scenarios
+
+    def max_load_length(self):
+        return max(self.loan_terms_in_years)
 
 
 class ScenarioTester(object):
@@ -154,9 +191,11 @@ class ScenarioTestResults(object):
         print(self.summary_str(
             'Monthly repayment', self.amount_repayed[:, 1]))
         print(self.summary_str(
-            'Monthly revenue', self.cumulative_revenue[:, 1]))
+            'Monthly revenue',
+            self.cumulative_revenue[:, -1] - self.cumulative_revenue[:, -2]))
         print(self.summary_str(
-            'Monthly profit', self.cumulative_profit[:, 1]))
+            'Monthly profit',
+            self.cumulative_profit[:, -1] - self.cumulative_profit[:, -2]))
         print(self.summary_str(
             'Total profit', self.cumulative_profit[:, -1]))
 
